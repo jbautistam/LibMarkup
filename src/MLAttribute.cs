ba14a52436bb5a7232f8bfdbb25c9f5eefba0665
jbautistam <jbautistam@gmail.com>
2018-12-23 13:32:10 +0100

    Updating modules

--- src/MLAttribute.cs
+++ src/MLAttribute.cs
@@ -3,7 +3,8 @@
 namespace Bau.Libraries.LibMarkupLanguage
 {
 	/// <summary>
-	///		Datos de un atributo
+	///		Datos de un atributo modificado en un módulo
+	///		
 	/// </summary>
 	public class MLAttribute : MLItemBase
 	{
