00858a61386aecbda0261076bd03cf03ad628262
jbautistam <jbautistam@gmail.com>
2018-12-23 12:39:34 +0100

    First version

---
+++ src/Services/ParserException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bau.Libraries.LibMarkupLanguage.Services
+{
+	/// <summary>
+	///		Excepciones del intérprete
+	/// </summary>
+	public class ParserException : Exception
+	{
+		public ParserException() {}
+
+		public ParserException(string message) : base(message) { }
+
+		public ParserException(string message, Exception innerException) : base(message, innerException) { }
+		
+		protected ParserException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context) {}
+	}
+}
