3693baf0bd37863864eb61f75a913c43bf925e35
Jose Antonio Bautista Montejo <jbautistam@gmail.com>
2023-10-22 14:09:30 +0200

    ToDo files

---
+++ src/Builders/MLNodeBuilder.cs
@@ -0,0 +1,28 @@
+﻿namespace Bau.Libraries.LibMarkupLanguage.Builders
+{
+	/// <summary>
+	///		Generador de nodos
+	/// </summary>
+	public class MLNodeBuilder
+	{
+		public MLNodeBuilder(string name, MLNodeBuilder parent = null)
+		{
+			Parent = parent;
+		}
+
+		/// <summary>
+		///		Nodos
+		/// </summary>
+		public MLNodesCollection Nodes { get; } = new MLNodesCollection();
+
+		/// <summary>
+		///		Genera un nodo
+		/// </summary>
+		private MLNode LastNode => Nodes[0];
+
+		/// <summary>
+		///		Generador padre
+		/// </summary>
+		private MLNodeBuilder Parent { get; }
+	}
+}
